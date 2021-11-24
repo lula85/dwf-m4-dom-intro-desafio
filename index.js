44c6c94d33c4a84490d0410818c8c06c5589b416
Lucia Dedyn <luciadedyn@MacBook-Pro-de-Lucia.local>
2021-11-24 18:24:18 -0300

    desafio terminado

--- index.js
+++ index.js
@@ -25,6 +25,20 @@ const cosasQueAprendimos = [
   },
 ];
 
-function main() {}
+function main() {
+  const listaEl = document.querySelector(".lista");
+  const itemsLi = document.querySelectorAll(".lista li");
+
+  for (const el of itemsLi) {
+    el.remove();
+  }
+  for (const item of cosasQueAprendimos) {
+    const newLiEl = document.createElement("li");
+    newLiEl.textContent = item.tema;
+    const claseAsignada = item.class || "item";
+    newLiEl.classList.add(claseAsignada);
+    listaEl.appendChild(newLiEl);
+  }
+}
 
 main();
